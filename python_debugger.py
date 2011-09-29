c9675fb0764b89760eab2fa6d04916afb09043bc
Nick Lang <nick.lang@gmail.com>
2011-09-29 16:12:17 -0600

    fixing external command to work with slt2 api

--- python_debugger.py
+++ python_debugger.py
@@ -1,36 +1,36 @@
-import sublime, sublimeplugin
+import sublime
+import sublime_plugin
 import subprocess
 
 BREAK_POINT = "ipdb.set_trace()  ################## Break Point ######################"
 
+
 class DebugCommand(sublime_plugin.TextCommand):
     """Command to automatically import ipdb and set_trace at the current line"""
     def run(self, edit):
-        print edit.__dict__
         # current position to place break point
         current_pos = self.view.sel()[0]
         current_line = self.view.line(current_pos)
         current_string = self.view.substr(current_line)
         tab_count = len(current_string) - len(current_string.lstrip())
-        
-        hard_tabs=False
+
+        hard_tabs = False
         if current_string[0] == "\t":
-            hard_tabs=True
+            hard_tabs = True
 
         if hard_tabs:
-            tabs = "\t"*tab_count
+            tabs = "\t" * tab_count
         else:
-            tabs = " "*tab_count
-        
-        self.view.insert(edit, current_line.begin(), u'%s%s\n' % (tabs, BREAK_POINT)) 
-        
-        first_line = self.view.line(self.view.text_point(0,0))
+            tabs = " " * tab_count
+
+        self.view.insert(edit, current_line.begin(), u'%s%s\n' % (tabs, BREAK_POINT))
+
+        first_line = self.view.line(self.view.text_point(0, 0))
         line_contents = self.view.substr(first_line)
         # Check if import ipdb is already the first line
         if line_contents != "import ipdb":
             # python debugger is not imported yet so make line1 of file the debugger import statement
-            self.view.insert(edit, first_line.begin(), u'import ipdb\n') 
-        
+            self.view.insert(edit, first_line.begin(), u'import ipdb\n')
 
 
 class UndebugCommand(sublime_plugin.TextCommand):
@@ -49,41 +49,41 @@ class UndebugCommand(sublime_plugin.TextCommand):
         debug_line = self.find_debug_line(sublime.Region(0, self.view.size()))
         while debug_line:
             self.view.erase(edit, debug_line)
-            debug_line=self.find_debug_line(sublime.Region(0, self.view.size()))
+            debug_line = self.find_debug_line(sublime.Region(0, self.view.size()))
 
-    
     def find_debug_line(self, region_set):
         for line in self.view.split_by_newlines(region_set):
             if self.view.substr(line).strip() in ["import ipdb", BREAK_POINT]:
                 return self.view.full_line(line)
         return None
-        
 
-class RunExternalCommand(sublimeplugin.TextCommand):
+
+class RunExternalCommand(sublime_plugin.TextCommand):
     """
     Runs an external command with the selected text,
     which will then be replaced by the command output.
     """
-    
-    def run(self, view, args):
-        if view.sel()[0].empty():
+
+    def run(self, edit):
+        if self.view.sel()[0].empty():
             # nothing selected: process the entire file
-            region = sublime.Region(0L, view.size())
+            region = sublime.Region(0L, self.view.size())
         else:
             # process only selected region
-            region = view.line(view.sel()[0])
+            region = self.view.sel()[0]
 
-        p = subprocess.Popen(
-            args,
-            shell   = True,
-            bufsize = -1,
-            stdout  = subprocess.PIPE,
-            stderr  = subprocess.PIPE,
-            stdin   = subprocess.PIPE)
+        cmd = self.view.substr(region)
 
-        output, error = p.communicate(view.substr(region).encode('utf-8'))
+        p = subprocess.Popen(cmd,
+            shell=True,
+            bufsize=-1,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE)
+
+        output, error = p.communicate(self.view.substr(region).encode('utf-8'))
 
         if error:
-            sublime.errorMessage(error.decode('utf-8'))
+            sublime.error_message(error.decode('utf-8'))
         else:
-            view.replace(region, output.decode('utf-8'))
+            self.view.replace(edit, region, output.decode('utf-8'))
