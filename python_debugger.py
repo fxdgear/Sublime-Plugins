353c708b6fda309b95f0bfbcc6f0c61ae1b6bbf4
Nick Lang <nick.lang@gmail.com>
2011-09-29 14:59:31 -0600

    pep8ing the breakpoint and adding a new class RunExternal command

--- python_debugger.py
+++ python_debugger.py
@@ -1,6 +1,7 @@
-import sublime, sublime_plugin
+import sublime, sublimeplugin
+import subprocess
 
-BREAK_POINT = "ipdb.set_trace() ################## Break Point ######################"
+BREAK_POINT = "ipdb.set_trace()  ################## Break Point ######################"
 
 class DebugCommand(sublime_plugin.TextCommand):
     """Command to automatically import ipdb and set_trace at the current line"""
@@ -56,5 +57,33 @@ class UndebugCommand(sublime_plugin.TextCommand):
             if self.view.substr(line).strip() in ["import ipdb", BREAK_POINT]:
                 return self.view.full_line(line)
         return None
+        
 
+class RunExternalCommand(sublimeplugin.TextCommand):
+    """
+    Runs an external command with the selected text,
+    which will then be replaced by the command output.
+    """
+    
+    def run(self, view, args):
+        if view.sel()[0].empty():
+            # nothing selected: process the entire file
+            region = sublime.Region(0L, view.size())
+        else:
+            # process only selected region
+            region = view.line(view.sel()[0])
 
+        p = subprocess.Popen(
+            args,
+            shell   = True,
+            bufsize = -1,
+            stdout  = subprocess.PIPE,
+            stderr  = subprocess.PIPE,
+            stdin   = subprocess.PIPE)
+
+        output, error = p.communicate(view.substr(region).encode('utf-8'))
+
+        if error:
+            sublime.errorMessage(error.decode('utf-8'))
+        else:
+            view.replace(region, output.decode('utf-8'))
